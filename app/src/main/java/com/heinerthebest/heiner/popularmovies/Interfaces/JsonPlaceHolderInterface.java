bf70253969f59c713ed3ce061f7cd5de65ab0851
heiner <heinerbauza@gmail.com>
2018-12-31 20:01:21 -0500

    Ready for get the reviews

--- app/src/main/java/com/heinerthebest/heiner/popularmovies/Interfaces/JsonPlaceHolderInterface.java
+++ app/src/main/java/com/heinerthebest/heiner/popularmovies/Interfaces/JsonPlaceHolderInterface.java
@@ -1,10 +1,18 @@
 package com.heinerthebest.heiner.popularmovies.Interfaces;
 
-import com.heinerthebest.heiner.popularmovies.models.Query;
+import android.util.Log;
+
+import com.heinerthebest.heiner.popularmovies.models.QueryForMovies;
+import com.heinerthebest.heiner.popularmovies.models.QueryForReviews;
+import com.heinerthebest.heiner.popularmovies.models.QueryForTrailers;
+import com.heinerthebest.heiner.popularmovies.models.Trailer;
 import com.heinerthebest.heiner.popularmovies.utilities.Constant;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JsonPlaceHolderInterface
 {
@@ -12,10 +20,14 @@ public interface JsonPlaceHolderInterface
     Constant constant = new Constant();
 
     @GET("popular?"+ Constant.PARAM_KEY +"="+ Constant.KEY)
-    Call<Query> getMovieByPopular();
+    Call<QueryForMovies> getMovieByPopular();
 
     @GET("top_rated?"+ Constant.PARAM_KEY +"="+ Constant.KEY)
-    Call<Query> getMovieByRated();
+    Call<QueryForMovies> getMovieByRated();
 
+    @GET("{id}/videos?"+ Constant.PARAM_KEY +"="+ Constant.KEY)
+    Call<QueryForTrailers>getTrailersOfMovie(@Path("id") int id_movie);
 
+    @GET("{movie_id}/reviews?"+ Constant.PARAM_KEY +"="+ Constant.KEY)
+    Call<QueryForReviews>getReviewsOfMovie(@Path("movie_id") int id);
 }
